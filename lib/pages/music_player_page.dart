5a03dbe121e7658615316d01efa4b3411396c884
alanaraujo902 <alanda022744@gmail.com>
2025-05-16 16:14:51 -0300

    1.20 - paginia inicial melhorada

--- lib/pages/music_player_page.dart
+++ lib/pages/music_player_page.dart
@@ -1,16 +1,15 @@
 import 'dart:io';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:filesystem_picker/filesystem_picker.dart';
-//import '../controllers/music_controller.dart';
 import '../controllers/music/music_controller.dart';
 
 import '../models/playlist.dart';
 import '../models/local_song.dart';
 import '../services/playlist_service.dart';
 import '../widgets/song_list_widget.dart';
-import '../widgets/playlist_card.dart';
-import '../dialogs/save_playlist_dialog.dart';
-import '../dialogs/add_to_playlist_dialog.dart';
+
+enum SortOption { alphabetical, reverseAlphabetical, byChecked, uncheckedFirst }
 
 class MusicPlayerPage extends StatefulWidget {
   @override
@@ -21,6 +20,7 @@ class _MusicPlayerPageState extends State<MusicPlayerPage> {
   final controller = MusicController();
   final PlaylistService playlistService = PlaylistService();
   bool hasSongs = false;
+  SortOption? _currentSort;
 
   @override
   void initState() {
@@ -45,51 +45,74 @@ class _MusicPlayerPageState extends State<MusicPlayerPage> {
 
     if (path != null) {
       await controller.loadSongsFromDirectory(path);
+      _applyCurrentSort();
       setState(() {
         hasSongs = controller.songs.isNotEmpty;
       });
     }
   }
 
-  void _savePlaylist() {
-    showSavePlaylistDialog(
-      context: context,
-      songs: controller.songs,
-      onSaved: (playlist) async {
-        final all = await playlistService.loadPlaylists();
-        all.add(playlist);
-        await playlistService.savePlaylists(all);
-        controller.playlistsNotifier.value = List.from(all);
-      },
-    );
+  void _applyCurrentSort() {
+    if (_currentSort != null) _sortSongs(_currentSort!);
   }
 
-  void _addToPlaylist(Playlist playlist) {
-    showAddToPlaylistDialog(
-      context: context,
-      availableSongs: controller.songs,
-      playlist: playlist,
-      onUpdated: (updatedPlaylist) async {
-        final all = await playlistService.loadPlaylists();
-        final idx = all.indexWhere((p) => p.name == updatedPlaylist.name);
-        if (idx != -1) {
-          all[idx] = updatedPlaylist;
-          await playlistService.savePlaylists(all);
-          controller.playlistsNotifier.value = List.from(all);
-        }
-      },
-    );
-  }
-
-  void _loadPlaylist(Playlist playlist) async {
-    final all = await playlistService.loadPlaylists();
-    final updated = all.firstWhere((p) => p.name == playlist.name, orElse: () => playlist);
-
-    controller.loadPlaylist(updated);
-
+  void _sortSongs(SortOption option) {
     setState(() {
-      hasSongs = updated.songs.isNotEmpty;
+      _currentSort = option;
+      switch (option) {
+        case SortOption.alphabetical:
+          controller.songs.sort((a, b) => a.title.toLowerCase().compareTo(b.title.toLowerCase()));
+          break;
+        case SortOption.reverseAlphabetical:
+          controller.songs.sort((a, b) => b.title.toLowerCase().compareTo(a.title.toLowerCase()));
+          break;
+        case SortOption.byChecked:
+          controller.songs.sort((a, b) => b.isChecked.toString().compareTo(a.isChecked.toString()));
+          break;
+        case SortOption.uncheckedFirst:
+          controller.songs.sort((a, b) => a.isChecked.toString().compareTo(b.isChecked.toString()));
+          break;
+      }
     });
+  }
+
+  void _savePlaylist() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        final nameController = TextEditingController();
+        return AlertDialog(
+          title: Text("Nome da Playlist"),
+          content: TextField(
+            controller: nameController,
+            decoration: InputDecoration(hintText: "Digite o nome"),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text("Cancelar"),
+            ),
+            TextButton(
+              onPressed: () async {
+                final name = nameController.text.trim();
+                if (name.isNotEmpty) {
+                  final playlist = Playlist(
+                    name: name,
+                    songs: List<LocalSong>.from(controller.songs),
+                  );
+                  final all = await playlistService.loadPlaylists();
+                  all.add(playlist);
+                  await playlistService.savePlaylists(all);
+                  controller.playlistsNotifier.value = List.from(all);
+                  Navigator.pop(context);
+                }
+              },
+              child: Text("Salvar"),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -135,44 +158,70 @@ class _MusicPlayerPageState extends State<MusicPlayerPage> {
           children: [
             ElevatedButton.icon(
               icon: Icon(Icons.folder_open),
-              label: Text('Selecionar Músicas'),
+              label: Text('Selecionar Pasta'),
               onPressed: _selectDirectory,
+            ),
+            SizedBox(height: 10),
+            ElevatedButton.icon(
+              icon: Icon(Icons.audiotrack),
+              label: Text("Selecionar Arquivos MP3"),
+              onPressed: () async {
+                final result = await FilePicker.platform.pickFiles(
+                  type: FileType.custom,
+                  allowedExtensions: ['mp3'],
+                  allowMultiple: true,
+                );
+
+                if (result != null) {
+                  final files = result.paths.whereType<String>().toList();
+                  final newSongs = <LocalSong>[];
+
+                  for (final path in files) {
+                    final dur = await controller.audioService.fetchDuration(Uri.file(path).toString());
+                    final song = LocalSong(
+                      id: path.hashCode,
+                      title: path.split('/').last,
+                      artist: 'Desconhecido',
+                      uri: Uri.file(path).toString(),
+                      durationMillis: dur?.inMilliseconds ?? 0,
+                    );
+                    newSongs.add(song);
+                  }
+
+                  controller.songs = newSongs;
+                  _applyCurrentSort();
+                  controller.currentSongNotifier.value = null;
+                  controller.currentSongIndex = -1;
+                  controller.currentSong = null;
+
+                  setState(() {
+                    hasSongs = newSongs.isNotEmpty;
+                  });
+                }
+              },
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Ordenar por:", style: TextStyle(fontWeight: FontWeight.bold)),
+                PopupMenuButton<SortOption>(
+                  icon: Icon(Icons.sort),
+                  onSelected: _sortSongs,
+                  itemBuilder: (context) => [
+                    PopupMenuItem(value: SortOption.alphabetical, child: Text('A-Z')),
+                    PopupMenuItem(value: SortOption.reverseAlphabetical, child: Text('Z-A')),
+                    PopupMenuItem(value: SortOption.byChecked, child: Text('Ouvidas por último')),
+                    PopupMenuItem(value: SortOption.uncheckedFirst, child: Text('Não ouvidas primeiro')),
+                  ],
+                ),
+              ],
             ),
             SizedBox(height: 10),
             ElevatedButton.icon(
               icon: Icon(Icons.save),
               label: Text("Salvar Playlist"),
               onPressed: hasSongs ? _savePlaylist : null,
-            ),
-            SizedBox(height: 10),
-            Text("Playlists Salvas", style: TextStyle(fontWeight: FontWeight.bold)),
-            SizedBox(
-              height: 200,
-              child: ValueListenableBuilder<List<Playlist>>(
-                valueListenable: controller.playlistsNotifier,
-                builder: (context, playlists, _) {
-                  return ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: playlists.length,
-                    itemBuilder: (context, index) {
-                      return PlaylistCard(
-                        playlist: playlists[index],
-                        onAddSongs: _addToPlaylist,
-                        onLoad: _loadPlaylist,
-                        onRemoveSongs: (updated) async {
-                          final all = await playlistService.loadPlaylists();
-                          final idx = all.indexWhere((p) => p.name == updated.name);
-                          if (idx != -1) {
-                            all[idx] = updated;
-                            await playlistService.savePlaylists(all);
-                            controller.playlistsNotifier.value = List.from(all);
-                          }
-                        },
-                      );
-                    },
-                  );
-                },
-              ),
             ),
             SizedBox(height: 20),
             if (hasSongs)
